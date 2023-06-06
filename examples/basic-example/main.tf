85a964e9365c56386ecc24f991f99846a0a27b57
janvt <janvt@me.com>
2023-06-06 17:23:24 +0200

    add basic example

---
+++ examples/basic-example/main.tf
@@ -0,0 +1,11 @@
+module "basic-example" {
+  source = "../../"
+
+  name = var.name
+
+  vpc_id = var.vpc_id
+  vpc_private_subnets = var.vpc_private_subnets
+  vpc_public_subnets = var.vpc_public_subnets
+
+  app_port = var.app_port
+}
---
+++ examples/basic-example/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  description = "Name to use for components."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "Id of VPC."
+  type        = string
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets from VPC."
+  type        = list(string)
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets from VPC."
+  type        = list(string)
+}
+
+variable "app_port" {
+  description = "Port that the app container listens on."
+  type        = number
+}
+
+variable "desired_count" {
+  default     = 1
+  description = "Number of desired Fargate tasks."
+  type        = number
+}
